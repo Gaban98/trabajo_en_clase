09b3692019d607cb31caeb95ab71b2822ecd7daf
Gaban98 <csrivera00@misena.edu.co>
2023-03-18 08:23:36 -0500

    funcionalidades en los templates

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path('home/', views.home, name='home'),
-# ]
+urlpatterns = [
+    path('category/', views.ViewCategory, name='ViewCategory'),
+    path('product/<int:idCategory>', views.ViewProductCategory, name='ViewProductCategory'),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+
+def ViewCategory(request):
+    ListCategory = Category.objects.all()
+    context = {'caterias': ListCategory, 'titulo': 'Categorias de los productos }'}
+    return render(request, 'products/category.html', context)
+
+def ViewProductCategory(request, idCategory):
+    NameCategory = Category.objects.get(id=idCategory)
+    ListProduct = Product.objects.filter(category_product=idCategory)
+    context = {'productos': ListProduct, 'titulo': 'Productos de la categoria: ' + NameCategory.description}
+    return render(request, 'products/product.html', context)
